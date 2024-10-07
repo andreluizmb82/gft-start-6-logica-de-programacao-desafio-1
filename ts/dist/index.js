9c6bad877ded920fb6f1c45c8e189d1e594f8e94
Andre Luiz Moura Morges <andreluizmb82@gmail.com>
2024-10-06 22:59:52 -0300

    feat(build): incluir pasta dist no controle de versão para facilitar a distribuição

---
+++ ts/dist/index.js
@@ -0,0 +1,21 @@
+console.log("Bem vindo ao desafio 1");
+import input from "./input.js";
+import solucao1 from "./solucao-1.js";
+import solucao2 from "./solucao-2.js";
+async function app() {
+    let solucaoASerExecutada;
+    do {
+        solucaoASerExecutada = await input("Digite 1 para executar a solução 1 ou 2 para executar a solução 2: ");
+        if (solucaoASerExecutada === "1") {
+            solucao1();
+        }
+        else if (solucaoASerExecutada === "2") {
+            solucao2();
+        }
+        else {
+            console.log("Opção inválida. Por favor, digite 1 ou 2.");
+            solucaoASerExecutada = "0";
+        }
+    } while (solucaoASerExecutada === "0");
+}
+app();
---
+++ ts/dist/input.js
@@ -0,0 +1,14 @@
+import readline from "readline";
+const input = (textoPergunta) => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    return new Promise((resolve) => {
+        rl.question(textoPergunta, (entrada) => {
+            rl.close();
+            resolve(entrada);
+        });
+    });
+};
+export default input;
